ae1119ad4851d471d545bdd79dfd9d85280bed19
Chukaev Roman <roman.chukaev1@gmail.com>
2026-02-18 22:45:46 +0700

    repair qrcode error

--- frontend/src/pages/FriendsPage.tsx
+++ frontend/src/pages/FriendsPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { friendsApi } from '../services/api'
 import type { Friendship } from '../types'
-import QRCode from 'qrcode'
+import * as QRCode from 'qrcode'
 import './FriendsPage.css'
 import './FeedPage.css'
 
